ff6f32721fdc8037db08f111a07ffe5da7293d77
nick23447 <156142540+nick23447@users.noreply.github.com>
2025-07-07 13:00:52 -0700

    added dummy posts

--- musicblog/routes.py
+++ musicblog/routes.py
@@ -1,9 +1,26 @@
 from musicblog import app
-from flask import render_template
+from flask import render_template, url_for
+
+
+posts = [
+     {
+          'author': 'John Doe',
+          'title': 'Music Post 1',
+          'content': 'First post content',
+          'date_posted': 'April 20, 2018'
+
+     },
+     {
+          'author': 'Jane Doe',
+          'title': 'Music Post 2',
+          'content': 'Second post content',
+          'date_posted': 'April 21, 2018'
+     }
+]
 
 
 @app.route('/')
 @app.route('/home')
 def home():
-    return render_template('/home.html')
+    return render_template('/home.html', posts=posts)
 
