b690fc13f33f47c90201ada269d237ca377b60c5
nick23447 <156142540+nick23447@users.noreply.github.com>
2025-07-08 20:32:50 -0700

    initalized sqlite

--- musicblog/__init__.py
+++ musicblog/__init__.py
@@ -1,5 +1,11 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '2d098706941decad765cb8fff79fcb68'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+
 
 from musicblog import routes
+
