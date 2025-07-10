d1b223ef6fc4b885dca1f9e6c54c9e507706d3b9
nick23447 <156142540+nick23447@users.noreply.github.com>
2025-07-10 16:18:56 -0700

    initialized flask_login for sessions

--- musicblog/__init__.py
+++ musicblog/__init__.py
@@ -1,12 +1,15 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '2d098706941decad765cb8fff79fcb68'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
 
 from musicblog import routes
 
