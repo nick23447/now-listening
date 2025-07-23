e028ca1298865adbee90813d85cb9401bb1965bf
nick23447 <156142540+nick23447@users.noreply.github.com>
2025-07-23 13:44:01 -0700

    add logic to allow user to change username and pw

--- musicblog/routes.py
+++ musicblog/routes.py
@@ -1,6 +1,6 @@
 from flask import render_template, url_for, flash, redirect, request
 from musicblog import app, db, bcrypt
-from musicblog.forms import RegistrationForm, LoginForm
+from musicblog.forms import RegistrationForm, LoginForm, UpdateAccountForm
 from musicblog.models import User, Post
 from flask_login import login_user, current_user, logout_user, login_required
 
@@ -30,7 +30,7 @@ def home():
 
 @app.route('/register', methods=['GET','POST'])
 def register():
-    if current_user.is_autheticated:
+    if current_user.is_authenticated:
         return redirect(url_for('home'))
     
     form = RegistrationForm()
@@ -71,8 +71,20 @@ def logout():
     return redirect(url_for('home'))
 
 
-@app.route('/account')
+@app.route('/account', methods=['GET','POST'])
 @login_required
 def account():
+    form = UpdateAccountForm()
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.email = form.email.data
+        db.session.commit()
+        flash('Your account has been updated!', 'success')
+        return redirect(url_for('account'))
+    
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.email.data = current_user.email
+
     image_file = url_for('static', filename='profile_pics/' + current_user.image_file)
-    return render_template('account.html', title='Account', image_file=image_file)
+    return render_template('account.html', title='Account', image_file=image_file, form=form)
