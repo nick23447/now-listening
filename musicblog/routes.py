570bf16cb0fe07b33ddd1046c4f50dfad923b8d9
nick23447 <156142540+nick23447@users.noreply.github.com>
2025-07-29 14:00:59 -0700

    delete old profile pic after new one is uploaded

--- musicblog/routes.py
+++ musicblog/routes.py
@@ -29,84 +29,88 @@ posts = [
 @app.route('/')
 @app.route('/home')
 def home():
-    return render_template('home.html', posts=posts)
+	return render_template('home.html', posts=posts)
 
 
 @app.route('/register', methods=['GET','POST'])
 def register():
-    if current_user.is_authenticated:
-        return redirect(url_for('home'))
+	if current_user.is_authenticated:
+		return redirect(url_for('home'))
     
-    form = RegistrationForm()
+	form = RegistrationForm()
 
-    if form.validate_on_submit():
-        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
-        user = User(username=form.username.data, email=form.email.data, password=hashed_password)
-        db.session.add(user)
-        db.session.commit()
-        flash(f"Your account has been created! You are now able to log in!", 'success')
-        return redirect(url_for('login'))
+	if form.validate_on_submit():
+		hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+		user = User(username=form.username.data, email=form.email.data, password=hashed_password)
+		db.session.add(user)
+		db.session.commit()
+		flash(f"Your account has been created! You are now able to log in!", 'success')
+		return redirect(url_for('login'))
     
-    return render_template('register.html', title='Register', form=form)
+	return render_template('register.html', title='Register', form=form)
 
 
 @app.route('/login', methods=['GET','POST'])
 def login():
-    if current_user.is_authenticated:
-        return redirect(url_for('home'))
+	if current_user.is_authenticated:
+		return redirect(url_for('home'))
     
-    form = LoginForm()
+	form = LoginForm()
 
-    if form.validate_on_submit():
-          user = User.query.filter_by(email=form.email.data).first()
-          if user and bcrypt.check_password_hash(user.password, form.password.data):
-              login_user(user, remember=form.remember.data)
-              next_page = request.args.get('next')
-              return redirect(next_page) if next_page else redirect(url_for('home'))
+	if form.validate_on_submit():
+		user = User.query.filter_by(email=form.email.data).first()
+		if user and bcrypt.check_password_hash(user.password, form.password.data):
+			login_user(user, remember=form.remember.data)
+			next_page = request.args.get('next')
+			return redirect(next_page) if next_page else redirect(url_for('home'))
 
-          flash('Login Unsuccessful. Please check username and password', 'danger')
+		flash('Login Unsuccessful. Please check username and password', 'danger')
      
-    return render_template('login.html', title='Login', form=form)
+	return render_template('login.html', title='Login', form=form)
 
 
 @app.route('/logout')
 def logout():
-    logout_user()
-    return redirect(url_for('home'))
+	logout_user()
+	return redirect(url_for('home'))
 
 
 def save_picture(form_picture):
-    random_hex = secrets.token_hex(8)
-    f_name, f_ext = os.path.splitext(form_picture.filename)
-    picture_fn = random_hex + f_ext
-    picture_path = os.path.join(app.root_path, 'static/profile_pics', picture_fn)
+	random_hex = secrets.token_hex(8)
+	f_name, f_ext = os.path.splitext(form_picture.filename)
+	picture_fn = random_hex + f_ext
+	picture_path = os.path.join(app.root_path, 'static/profile_pics', picture_fn)
 
-    outpute_size = (125,125)
-    i = Image.open(form_picture)
-    i.thumbnail(outpute_size)
-    i.save(picture_path)
+	outpute_size = (125,125)
+	i = Image.open(form_picture)
+	i.thumbnail(outpute_size)
+	i.save(picture_path)
     
-    return picture_fn
+	return picture_fn
 
 
 @app.route('/account', methods=['GET','POST'])
 @login_required
 def account():
-    form = UpdateAccountForm()
-    if form.validate_on_submit():
-     if form.picture.data:
-          picture_file = save_picture(form.picture.data)
-          current_user.image_file = picture_file
+	form = UpdateAccountForm()
+	if form.validate_on_submit():
+		if form.picture.data:
+			old_pic = current_user.image_file
+			picture_file = save_picture(form.picture.data)
+			current_user.image_file = picture_file
+			if old_pic != 'default.jpg':
+				os.remove(os.path.join(app.root_path, 'static/profile_pics', old_pic))
+			current_user.image_file = picture_file
 
-     current_user.username = form.username.data
-     current_user.email = form.email.data
-     db.session.commit()
-     flash('Your account has been updated!', 'success')
-     return redirect(url_for('account'))
-    
-    elif request.method == 'GET':
-        form.username.data = current_user.username
-        form.email.data = current_user.email
+		current_user.username = form.username.data
+		current_user.email = form.email.data
+		db.session.commit()
+		flash('Your account has been updated!', 'success')
+		return redirect(url_for('account'))
+		
+	elif request.method == 'GET':
+		form.username.data = current_user.username
+		form.email.data = current_user.email
 
-    image_file = url_for('static', filename='profile_pics/' + current_user.image_file)
-    return render_template('account.html', title='Account', image_file=image_file, form=form)
+	image_file = url_for('static', filename='profile_pics/' + current_user.image_file)
+	return render_template('account.html', title='Account', image_file=image_file, form=form)
