eec8ddf83d2bf050e2477745c29a6412b9119034
nick23447 <156142540+nick23447@users.noreply.github.com>
2025-07-29 13:43:33 -0700

    allow user to change their profile pic

--- musicblog/routes.py
+++ musicblog/routes.py
@@ -1,8 +1,11 @@
+import os
+import secrets
 from flask import render_template, url_for, flash, redirect, request
 from musicblog import app, db, bcrypt
 from musicblog.forms import RegistrationForm, LoginForm, UpdateAccountForm
 from musicblog.models import User, Post
 from flask_login import login_user, current_user, logout_user, login_required
+
 
 
 posts = [
@@ -71,16 +74,30 @@ def logout():
     return redirect(url_for('home'))
 
 
+def save_picture(form_picture):
+    random_hex = secrets.token_hex(8)
+    f_name, f_ext = os.path.splitext(form_picture.filename)
+    picture_fn = random_hex + f_ext
+    picture_path = os.path.join(app.root_path, 'static/profile_pics', picture_fn)
+    form_picture.save(picture_path)
+    
+    return picture_fn
+
+
 @app.route('/account', methods=['GET','POST'])
 @login_required
 def account():
     form = UpdateAccountForm()
     if form.validate_on_submit():
-        current_user.username = form.username.data
-        current_user.email = form.email.data
-        db.session.commit()
-        flash('Your account has been updated!', 'success')
-        return redirect(url_for('account'))
+     if form.picture.data:
+          picture_file = save_picture(form.picture.data)
+          current_user.image_file = picture_file
+
+     current_user.username = form.username.data
+     current_user.email = form.email.data
+     db.session.commit()
+     flash('Your account has been updated!', 'success')
+     return redirect(url_for('account'))
     
     elif request.method == 'GET':
         form.username.data = current_user.username
