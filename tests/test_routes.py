28a4b6f378b718a4bda7b1f96c8c94ca10bec2b1
nick23447 <156142540+nick23447@users.noreply.github.com>
2025-08-22 13:53:38 -0700

    create tests folder

---
+++ tests/test_routes.py
@@ -0,0 +1,20 @@
+from musicblog import routes
+
+def test_home():
+    pass
+
+def test_register():
+    pass
+
+def test_login():
+    pass
+
+def test_logout():
+    pass
+
+def test_account():
+    pass
+
+def test_new_post():
+    pass
+    
