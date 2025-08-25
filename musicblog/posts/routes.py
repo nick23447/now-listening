d5720567c53ef48f70edd385506c172526f066c0
nick23447 <156142540+nick23447@users.noreply.github.com>
2025-08-25 16:34:49 -0700

    add in post routes

---
+++ musicblog/posts/routes.py
@@ -0,0 +1,17 @@
+from flask import Blueprint
+
+posts = Blueprint('posts', __name__)
+
+
+
+posts.route('/post/new', methods=['GET','POST'])
+@login_required
+def new_post():
+	form = PostForm()
+	if form.validate_on_submit():
+		post = Post(title=form.title.data, content=form.content.data, author=current_user)
+		db.session.add(post)
+		db.session.commit()
+		flash('Your post has been created!', 'success')
+		return redirect(url_for('home.html'))
+	return render_template('create_post.html', title='New Post', form=form)
