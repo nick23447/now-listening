f194cd1579ae478face19b86e262c235cb91dc2e
nick23447 <156142540+nick23447@users.noreply.github.com>
2025-08-25 18:34:00 -0700

    change configs to env variables

--- musicblog/__init__.py
+++ musicblog/__init__.py
@@ -2,15 +2,15 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from musicblog.config import Config
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '2d098706941decad765cb8fff79fcb68'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config.from_object(Config)
 
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
-login_manager.login_view = 'login'
+login_manager.login_view = 'users.login'
 login_manager.login_message_category = 'info'
 
 from musicblog.users.routes import users
