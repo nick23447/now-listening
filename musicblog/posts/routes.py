f11fa32fb5f1c98e88641ec646d5080df4fa390f
nick23447 <156142540+nick23447@users.noreply.github.com>
2025-08-25 18:08:47 -0700

    fix missing @ in front of routes bug

--- musicblog/posts/routes.py
+++ musicblog/posts/routes.py
@@ -6,7 +6,7 @@ from musicblog.posts.forms import PostForm
 
 posts = Blueprint('posts', __name__)
 
-posts.route('/post/new', methods=['GET','POST'])
+@posts.route('/post/new', methods=['GET','POST'])
 @login_required
 def new_post():
 	form = PostForm()
