1be73ccc8673ec4dd9674819aa2fd3ecdaef4eef
nick23447 <156142540+nick23447@users.noreply.github.com>
2025-08-25 18:08:28 -0700

    fix missing @ in front of routes bug

--- musicblog/main/routes.py
+++ musicblog/main/routes.py
@@ -3,8 +3,8 @@ from musicblog.models import Post
 
 main = Blueprint('main', __name__)
 
-main.route('/')
-main.route('/home')
+@main.route('/')
+@main.route('/home')
 def home():
 	posts = Post.query.all()
 	return render_template('home.html', posts=posts)
