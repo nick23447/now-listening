581c983d7742005156ec8d13e2ead39c9a487207
nick23447 <156142540+nick23447@users.noreply.github.com>
2025-09-20 16:23:10 -0700

    restructure app into function for multiple instance use

--- musicblog/__init__.py
+++ musicblog/__init__.py
@@ -4,20 +4,31 @@ from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from musicblog.config import Config
 
-app = Flask(__name__)
-app.config.from_object(Config)
 
-db = SQLAlchemy(app)
-bcrypt = Bcrypt(app)
-login_manager = LoginManager(app)
+db = SQLAlchemy()
+bcrypt = Bcrypt()
+login_manager = LoginManager()
 login_manager.login_view = 'users.login'
 login_manager.login_message_category = 'info'
 
-from musicblog.users.routes import users
-from musicblog.posts.routes import posts
-from musicblog.main.routes import main
 
-app.register_blueprint(users)
-app.register_blueprint(posts)
-app.register_blueprint(main)
+def create_app(config_class=Config):
+    app = Flask(__name__)
+    app.config.from_object(config_class)
+
+    db.init_app(app)
+    bcrypt.init_app(app)
+    login_manager.init_app(app)
+    login_manager.init_app(app) 
+    login_manager.init_app(app)
+
+    from musicblog.users.routes import users
+    from musicblog.posts.routes import posts
+    from musicblog.main.routes import main
+
+    app.register_blueprint(users)
+    app.register_blueprint(posts)
+    app.register_blueprint(main)
+
+    return app
 
