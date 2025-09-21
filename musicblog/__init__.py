fa7086673eb8db1b573f2fb151aeff09fdbbe5d6
nick23447 <156142540+nick23447@users.noreply.github.com>
2025-09-20 18:39:15 -0700

    add migrations for db

--- musicblog/__init__.py
+++ musicblog/__init__.py
@@ -3,9 +3,11 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from musicblog.config import Config
+from flask_migrate import Migrate
 
 
 db = SQLAlchemy()
+migrate = Migrate()
 bcrypt = Bcrypt()
 login_manager = LoginManager()
 login_manager.login_view = 'users.login'
@@ -18,6 +20,7 @@ def create_app(config_class=Config):
 
     db.init_app(app)
     bcrypt.init_app(app)
+    migrate.init_app(app, db)
     login_manager.init_app(app)
     login_manager.init_app(app) 
     login_manager.init_app(app)
