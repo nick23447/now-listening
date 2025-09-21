0ec367c6737ef0ca0e34d9e184b8f46488253055
nick23447 <156142540+nick23447@users.noreply.github.com>
2025-09-21 13:51:45 -0700

    validate hidden fields

--- musicblog/posts/forms.py
+++ musicblog/posts/forms.py
@@ -7,7 +7,9 @@ from wtforms.validators import DataRequired
 class PostForm(FlaskForm):
     title = StringField('Title', validators=[DataRequired()])
     content = TextAreaField('Content', validators=[DataRequired()])
-    album_name = HiddenField('Album Name')
-    album_artist = HiddenField('Album Artist')
-    album_image = HiddenField('Album Image')
+    album_name = HiddenField('Album Name', validators=[DataRequired()])
+    album_artist = HiddenField('Album Artist', validators=[DataRequired()])
+    album_image = HiddenField('Album Image', validators=[DataRequired()])
     submit = SubmitField('Post')
+
+    
