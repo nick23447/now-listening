71add24cd2753b91a3f0386241b54b8e57e329f7
nick23447 <156142540+nick23447@users.noreply.github.com>
2025-10-05 14:49:51 -0700

    add different instances to configure app

---
+++ musicblog/config.py
@@ -0,0 +1,11 @@
+import os
+
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+
+class TestConfig(Config):
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///:memory:'  
+    WTF_CSRF_ENABLED = False  # 
+    SECRET_KEY = 'test-secret-key'  
