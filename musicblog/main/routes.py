9d4553c9c345058030063b8fb83b5c84887d450c
nick23447 <156142540+nick23447@users.noreply.github.com>
2025-10-06 13:29:31 -0700

    order posts by date posted (descending)

--- musicblog/main/routes.py
+++ musicblog/main/routes.py
@@ -6,7 +6,7 @@ main = Blueprint('main', __name__)
 @main.route('/')
 @main.route('/home')
 def home():
-	posts = Post.query.all()
+	posts = Post.query.order_by(Post.date_posted.desc())
 	return render_template('home.html', posts=posts)
 
 
