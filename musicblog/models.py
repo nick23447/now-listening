9bb9c5219cffdb1e65ceeb504e248303b4581bf6
nick23447 <156142540+nick23447@users.noreply.github.com>
2025-10-10 18:56:48 -0700

    update deprecated User.query method

--- musicblog/models.py
+++ musicblog/models.py
@@ -5,7 +5,7 @@ from flask_login import UserMixin
 
 @login_manager.user_loader
 def load_user(user_id):
-    return User.query.get(int(user_id))
+    return db.session.get(User, int(user_id))
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
