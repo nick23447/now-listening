4220f86e9c330151e9323b3ac50c4aaf0fae2378
nick23447 <156142540+nick23447@users.noreply.github.com>
2025-10-11 17:42:51 -0700

    implement test data email change and add more login failure tests

--- tests/functional/test_users_routes.py
+++ tests/functional/test_users_routes.py
@@ -18,10 +18,10 @@ def test_user_registration(test_client, register_user):
     assert len(response.history) == 1
     assert response.request.path == "/login"
     
-    user = User.query.filter_by(email='new@example.com').first()
+    user = User.query.filter_by(email='test@example.com').first()
     assert user is not None
     assert user.username == 'newuser'
-    assert user.email == 'new@example.com'
+    assert user.email == 'test@example.com'
 
 def test_user_authenticated(test_client, login_user):
     """Test that logged-in users are redirected from login page"""
@@ -38,7 +38,7 @@ def test_user_registration_with_existing_email(test_client, register_user):
     response = test_client.post('/register', 
         data={
             'username': 'newuser1',
-            'email': 'new@example.com',
+            'email': 'test@example.com',
             'password': 'password123',
             'confirm_password': 'password123'
         },
@@ -69,7 +69,7 @@ def test_user_registration_pw_mismatch(test_client, init_database):
     response = test_client.post('/register', 
         data = {
             'username': 'newuser',
-            'email': 'new@example.com',
+            'email': 'test@example.com',
             'password': 'password123',
             'confirm_password': 'password321'
         },
@@ -102,16 +102,31 @@ def test_login_with_remember_me(test_client, register_user):
     response = test_client.post('/login',
         data={
             'email': 'test@example.com',
-            'password': 'testpassword123',
+            'password': 'password123',
             'remember': True
         },
         follow_redirects=True
     )
     
     assert response.status_code == 200
+    assert response.request.path == "/home"
+
+def test_login_with_wrong_email(test_client, register_user):
+    """Test login with incorrect email"""
+    response = test_client.post('/login',
+        data={
+            'email': 'new@example.com',
+            'password': 'testpassword123'
+        },
+        follow_redirects=True
+    )
+    
+    assert response.status_code == 200
+    assert b'Login Unsuccessful. Please check username and password' in response.data
+    assert response.request.path == "/login"
 
 def test_login_with_wrong_pw(test_client, register_user):
-    """Test login with remember me checked"""
+    """Test login with incorrect password"""
     response = test_client.post('/login',
         data={
             'email': 'test@example.com',
@@ -122,6 +137,9 @@ def test_login_with_wrong_pw(test_client, register_user):
     
     assert response.status_code == 200
     assert b'Login Unsuccessful. Please check username and password' in response.data
+    assert response.request.path == "/login"
+
+
 
 
     
