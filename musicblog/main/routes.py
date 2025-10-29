56e65de1cb0e5901b94dda952d36b38c84a7b1a4
nick23447 <156142540+nick23447@users.noreply.github.com>
2025-10-29 16:49:01 -0700

    change post order to pagination with 5 posts per page

--- musicblog/main/routes.py
+++ musicblog/main/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, Blueprint
+from flask import render_template, Blueprint, request
 from musicblog.models import Post
 
 main = Blueprint('main', __name__)
@@ -6,7 +6,7 @@ main = Blueprint('main', __name__)
 @main.route('/')
 @main.route('/home')
 def home():
-	posts = Post.query.order_by(Post.date_posted.desc())
+	page = request.args.get('page', 1, type=int)
+	posts = Post.query.paginate(page=page, per_page=5)
 	return render_template('home.html', posts=posts)
-
 
