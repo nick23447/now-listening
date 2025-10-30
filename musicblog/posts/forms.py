fd3c25746d4a4818444a677c674055c65478da48
nick23447 <156142540+nick23447@users.noreply.github.com>
2025-10-29 17:50:11 -0700

    add char limit to title and content

--- musicblog/posts/forms.py
+++ musicblog/posts/forms.py
@@ -1,12 +1,12 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, TextAreaField, SubmitField, HiddenField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Length
 
 
 
 class PostForm(FlaskForm):
-    title = StringField('Title', validators=[DataRequired()])
-    content = TextAreaField('Content', validators=[DataRequired()])
+    title = StringField('Title', validators=[DataRequired(), Length(max=50)])
+    content = TextAreaField('Content', validators=[DataRequired(), Length(max=1000)])
     album_name = HiddenField('Album Name', validators=[DataRequired(message="Please select an album from the search.")])
     album_artist = HiddenField('Album Artist', validators=[DataRequired()])
     album_image = HiddenField('Album Image', validators=[DataRequired()])
