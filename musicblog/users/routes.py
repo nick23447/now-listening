bb16ca2919594b3be3e9600c5c60a5cce59c2b74
nick23447 <156142540+nick23447@users.noreply.github.com>
2025-11-02 15:00:24 -0800

    add route to handle verifying pw reset tokens

--- musicblog/users/routes.py
+++ musicblog/users/routes.py
@@ -82,5 +82,20 @@ def account():
 def reset_request():
 	if current_user.is_authenticated:
 		return redirect(url_for('home'))
+	
 	form = RequestResetForm()
-	return render_template('reset_request.html', title='Reset Password', form=form)
+	return render_template('users.reset_request.html', title='Reset Password', form=form)
+
+
+@users.route("/reset_password/<token>", methods=['GET','POST'])
+def reset_token(token):
+	if current_user.is_authenticated:
+		return redirect(url_for('home'))
+	
+	user = User.verify_reset_token(token)
+	if not user:
+		flash('That is an invalid or expired token', "warning")
+		return redirect(url_for('users.reset_request'))
+	
+	form = ResetPasswordForm()
+	return render_template('users.reset_token.html', title='Reset Password', form=form)
