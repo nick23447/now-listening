0ec9b005153a676260e124c41f33cf26d615bcd6
nick23447 <156142540+nick23447@users.noreply.github.com>
2025-11-02 14:47:08 -0800

    add static method to User model to verify reset pw token

--- musicblog/models.py
+++ musicblog/models.py
@@ -20,7 +20,17 @@ class User(db.Model, UserMixin):
         s = Serializer(app.config['SECRET_KEY'], expires_sec)
         token = s.dumps({'user_id': self.id}).decode('utf-8')
         return token
-
+    
+    @staticmethod
+    def verify_reset_token(token):
+        s = Serializer(app.config['SECRET_KEY'])
+        try:
+            user_id = s.loads(token)['user_id']
+        except:
+            return None
+        
+        return User.query.get(user_id)
+    
     def __repr__(self):
         return f"{self.username}"
         #return f"User('{self.username}'), '{self.email}', '{self.image_file}')"
