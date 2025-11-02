ada3d0947f2141f3731926f0628742b5cf3d19b3
nick23447 <156142540+nick23447@users.noreply.github.com>
2025-11-02 15:33:26 -0800

    initalize Mail for pw resets

--- musicblog/__init__.py
+++ musicblog/__init__.py
@@ -4,15 +4,14 @@ from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from musicblog.config import Config
 from flask_migrate import Migrate
-
+from flask_mail import Mail
 
 db = SQLAlchemy()
 migrate = Migrate()
 bcrypt = Bcrypt()
 login_manager = LoginManager()
 login_manager.login_view = 'users.login'
-login_manager.login_message_category = 'info'
-
+mail = Mail()
 
 def create_app(config_class=Config):
     app = Flask(__name__)
@@ -22,7 +21,8 @@ def create_app(config_class=Config):
     bcrypt.init_app(app)
     migrate.init_app(app, db)
     login_manager.init_app(app)
-
+    mail.init_app(app)
+    
     from musicblog.users.routes import users
     from musicblog.posts.routes import posts
     from musicblog.main.routes import main
