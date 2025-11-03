6b8be5fbe20feaae5e6729d09130de0160c5b565
nick23447 <156142540+nick23447@users.noreply.github.com>
2025-11-02 17:10:16 -0800

    fix bug of same func names

--- musicblog/errors/handlers.py
+++ musicblog/errors/handlers.py
@@ -9,11 +9,11 @@ def error_404(error):
 
 
 @errors.app_errorhandler(403)
-def error_404(error):
+def error_403(error):
     return render_template('errors/404.html'), 403
 
 
 @errors.app_errorhandler(500)
-def error_404(error):
+def error_500(error):
     return render_template('errors/404.html'), 500
 
