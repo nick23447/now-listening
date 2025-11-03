36b55c4296ff571462a3e5607783e31efe71c47f
nick23447 <156142540+nick23447@users.noreply.github.com>
2025-11-03 15:52:59 -0800

    add rating option to form to allow user to input album rating

--- musicblog/posts/forms.py
+++ musicblog/posts/forms.py
@@ -1,12 +1,13 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, TextAreaField, SubmitField, HiddenField
-from wtforms.validators import DataRequired, Length
+from wtforms import StringField, TextAreaField, SubmitField, HiddenField, IntegerField
+from wtforms.validators import DataRequired, Length, NumberRange
 
 
 
 class PostForm(FlaskForm):
     title = StringField('Title', validators=[DataRequired(), Length(max=50)])
     content = TextAreaField('Content', validators=[DataRequired(), Length(max=1000)])
+    rating = IntegerField('Rating', validators=[DataRequired(), NumberRange(min=1, max=5, message="Rating must be between 1 and 5")])
     album_name = HiddenField('Album Name', validators=[DataRequired(message="Please select an album from the search.")])
     album_artist = HiddenField('Album Artist', validators=[DataRequired()])
     album_image = HiddenField('Album Image', validators=[DataRequired()])
