75c9557389f8d14278a38ae6acd7ec9966acd2cc
nick23447 <156142540+nick23447@users.noreply.github.com>
2025-11-02 17:00:44 -0800

    change smtp to gmail instead of google mail

--- musicblog/config.py
+++ musicblog/config.py
@@ -3,7 +3,7 @@ import os
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
-    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_SERVER = 'smtp.gmail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
     MAIL_USERNAME = os.environ.get('EMAIL_USER')
@@ -12,5 +12,6 @@ class Config:
 class TestConfig(Config):
     TESTING = True
     SQLALCHEMY_DATABASE_URI = 'sqlite:///:memory:'  
-    WTF_CSRF_ENABLED = False  # 
+    WTF_CSRF_ENABLED = False  
     SECRET_KEY = 'test-secret-key'  
+
